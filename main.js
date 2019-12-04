56a09b6992c4d718352736ab3a73eb39f6f126c4
Jenny_Ripper <jennifer.ripper@gmail.com>
2019-12-04 10:38:02 -0500

    new question card class creates with start of array holding class properties

--- main.js
+++ main.js
@@ -18,3 +18,26 @@ const quiz = function(quiz_name) {
     this.quiz_name
     this.questions = [];
 }
+
+function startQuiz() {
+
+    function showQuestions() {
+
+    }
+
+    function showResults() {
+
+    }
+}
+
+class questionCard {
+    constructor(question,answerChoice,rightAnswer) {
+        this.question = question
+        this.answerchoice = answerChoice
+        this.rightAnswer = rightAnswer
+    }
+}
+
+const parkCards = [
+    new questionCard
+]
