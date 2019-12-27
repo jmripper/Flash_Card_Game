0b463d7e920c33c7fb8be444d65e64257b222c41
Jenny_Ripper <jennifer.ripper@gmail.com>
2019-12-27 15:03:57 -0500

    API data correctly fetching

--- main.js
+++ main.js
@@ -2,63 +2,67 @@
 class questionCard {
     constructor(question,answerChoice,choiceImages,rightAnswer,rightAnswerInfo) {
         this.question = question
-        this.answerchoice = answerChoice
+        this.answerChoice = answerChoice
         this.choiceImages = choiceImages
         this.rightAnswer = rightAnswer
         this.rightAnswerInfo = rightAnswerInfo   
     }
+
     isAnswerCorrect(clickedAnswer) {
         if (clickedAnswer === this.rightAnswer) {
             return true
         } else {
             return false;}
     }
+
 }
 
 //new array with class questionCard and its properties, question, answerChoice, rightAnswer
-const parkCards = [
-    new questionCard("What state contains the most national parks?",
-    ["Alaska", "Colorado", "Utah", "California"], ['images/alaskaglag.png','images/coloradoflaf.png','images/utahflag.png','images/califroniaflag.png'],
-    "California", "With the creation of Pinnacles National Park in 2013, California broke its 8-8 tie with Alaska to become the state with the most national parks. In addition to its nine national parks, California also maintains over 20 National Park System-administered areas including multiple National Monuments, National Historic Trails, National Historic Sites and more."),
+const parkCardOne = new questionCard [{
+    "What state contains the most national parks?",
+    ["Alaska", "Colorado", "Utah", "California"], 
+    ['images/alaskaflag.png','images/coloradoflag.png','images/utahflag.png','images/californiaflag.png'],
+    "California", 
+    "With the creation of Pinnacles National Park in 2013, California broke its 8-8 tie with Alaska to become the state with the most national parks. In addition to its nine national parks, California also maintains over 20 National Park System-administered areas including multiple National Monuments, National Historic Trails, National Historic Sites and more."
+}];
 
-    new questionCard("What was the first National Park?",
-    ["Dealth Valley National Park", "Yosemite National Park", "Acadia National Park", "Yellowstone National Park"],
-    "Yellowstone National Park", "Yellowstone National Park, located in Wyoming, Montana and Idaho, was signed into law by President Ulysses S. Grant in 1872, becoming the first national park in America and in the world."),
+const parkCardTwo = new questionCard [{
+    question: "What was the first National Park?",
+    answerChoice: ["Dealth Valley National Park", "Yosemite National Park", "Acadia National Park", "Yellowstone National Park"],
+    choiceImages: ['images/utahflag.png'],
+    rightAnswer: "Yellowstone National Park", 
+    rightAnswerInfo: "Yellowstone National Park, located in Wyoming, Montana and Idaho, was signed into law by President Ulysses S. Grant in 1872, becoming the first national park in America and in the world.")],
 
-    new questionCard("What national park is home to the longest cave system in the world?",
-    ["Mammoth Cave National Park", "Canyonlands National Park", "Carlsbad Caverns National Park", "Great Basin National Park"], 
+    const parkCardThree = new questionCard("What national park is home to the longest cave system in the world?",
+    ["Mammoth Cave National Park", "Canyonlands National Park", "Carlsbad Caverns National Park", "Great Basin National Park"], [''],
     "Mammoth Cave National Park", "Mammoth Cave National Park in Kentucky is home to the world's longest known cave system, which measures more than 400 miles long. The park offers a variety of guided cave tours – including a ranger-led nature trek for kids – as well as camping, horseback riding, biking and other activities to explore the beauty of Kentucky's Green River Valley."),
 
-    new questionCard("What national park is home to the world's largest tree by volume?",
-    ["Red Wood National Park", "Joshua Tree National Park", "California's Sequoia National Park", "Everglades National Park"], 
+    const parkCardFour = new questionCard("What national park is home to the world's largest tree by volume?",
+    ["Red Wood National Park", "Joshua Tree National Park", "California's Sequoia National Park", "Everglades National Park"], [''],
     "California's Sequoia National Park", "The General Sherman Tree located in California's Sequoia National Park is the largest tree by volume in the world. This massive tree measures a whopping 275 feet tall and 25 feet wide, resulting in a trunk volume of 52,513 cubic feet. It is also one of the oldest trees on the planet with an estimated age of 2,300-2,700 years old."),
 
-    new questionCard("What is the most visited national park?",
-    ["The Great Smoky Mountains", "The Grand Canyon", "Yosemite National Park", "Rocky Mountain National Park"], 
+    const parkCardFive = new questionCard("What is the most visited national park?",
+    ["The Great Smoky Mountains", "The Grand Canyon", "Yosemite National Park", "Rocky Mountain National Park"], [''],
     "The Great Smoky Mountains", "In 2015 alone, parks across the U.S. drew more than 307.2 million visits. That's almost one visit per every person in America! Of these visits, over 10 million of them were to The Great Smoky Mountains – almost twice as much as the 5.5 million visits to the second most popular park, The Grand Canyon."),
 
-    new questionCard("What is the least visited national park?",
-    ["Dry Tortugas National Park", "Isle Royale National Park", "North Cascades National Park", "Black Canyon of the Gunnison National Park"], 
+    const parkCardSix = new questionCard("What is the least visited national park?",
+    ["Dry Tortugas National Park", "Isle Royale National Park", "North Cascades National Park", "Black Canyon of the Gunnison National Park"], [''],
     "Isle Royale National Park", "According to the National Park Service Visitor Statistics, Michigan's Isle Royale National Park saw only 18,684 recreational visits in 2015. This is due largely to the fact that the remote park is accessible only by boat or seaplane."),
 
-    new questionCard("What National Park contains the highest peak in North America?",
+    const parkCardSeven = new questionCard("What National Park contains the highest peak in North America?",
     ["Grand Teton National Park", "Hawaii Volcanoes National Park", "Mount Rainier National Park", "Denali National Park"], 
     ['/images/Denali.png'], 
     "Denali National Park", "Formerly known as Mount McKinley, Denali is the tallest mountain in North America with a peak elevation of 20,310 feet."),
 
-    new questionCard("Which is the smallest national park site?",
-    ["Belmont-Paul Women's Equality National Monument", "Thaddeus Kosciuszko National Memorial", "General Grant National Memorial", "African Burial Ground Monument"], "Thaddeus Kosciuszko National Memorial", "The smallest site in the National Park System, the Thaddeus Kościuszko National Memorial in downtown Philadelphia, honors a Polish freedom fighter who helped American colonists during the Revolutionary War and preserves the sweeping history of his life in only about 80 square meters of space."),
+    const parkCardEight = new questionCard("Which is the smallest national park site?",
+    ["Belmont-Paul Women's Equality National Monument", "Thaddeus Kosciuszko National Memorial", "General Grant National Memorial", "African Burial Ground Monument"], [''],
+    "Thaddeus Kosciuszko National Memorial", "The smallest site in the National Park System, the Thaddeus Kościuszko National Memorial in downtown Philadelphia, honors a Polish freedom fighter who helped American colonists during the Revolutionary War and preserves the sweeping history of his life in only about 80 square meters of space."),
 
-    new questionCard("Which was the first national park established for the purpose of protecting man-made structures?",
-    ["National Mall and Memorial Parks", "New River Gorge", "Mesa Verde National Park", "For Caroline National Park"], 
+    const parkCardNine = new questionCard("Which was the first national park established for the purpose of protecting man-made structures?",
+    ["National Mall and Memorial Parks", "New River Gorge", "Mesa Verde National Park", "For Caroline National Park"], [''],
     "Mesa Verde National Park", "In June 1906 President Theodore Roosevelt signed the bill that established Mesa Verde National Park in southwestern Colorado to protect Ancestral Puebloan archaeological sites."),
 
-    new questionCard("Which national park site contains the most lighthouses?",
-    ["Cape Cod National Seashore", "Pt. Reyes Natiaonl Seashores", "Ise Royale National Park", "Apostle Island National Lakeshore"], 
-    "Apostle Island National Lakeshore", "Nine of the 50 lighthouses cared for by the National Park Service are located within Apostle Islands National Lakeshore in Wisconsin, making it the national park site with the most lighthouses. All of them were built in the 19th century, and some are still in service today."),
-];
-
-const imageArray = ['images/alaskaflag.png', 'images/coloradoflag.png']
+const parkCardTen = new questionCard("Which national park site contains the most lighthouses?", ["Cape Cod National Seashore", "Pt. Reyes Natiaonl Seashores", "Ise Royale National Park", "Apostle Island National Lakeshore"], [''], "Apostle Island National Lakeshore", "Nine of the 50 lighthouses cared for by the National Park Service are located within Apostle Islands National Lakeshore in Wisconsin, making it the national park site with the most lighthouses. All of them were built in the 19th century, and some are still in service today.")
 
 let activeCard = 0
 let score = 0
@@ -79,9 +83,10 @@ function displayQuestionCard() {
         const card = parkCards[activeCard]
         const currentQuestion = card.question;
         const cardAnswerList = card.answerchoice
-        const imageChoice = card.choiceImages
+        const imageList = card.choiceImages
         questionTitle.innerHTML = currentQuestion
-        
+        //console.log(imageList)
+
         //creates next question button
         nextButton.setAttribute("class", "button-style")
         nextButton.innerText = "Next Question"
@@ -91,44 +96,42 @@ function displayQuestionCard() {
         } //else {
             //backButton.style.pointerEvents = "none";
         //}
+
+        //for (let i = 0; i < imageList.length; i++) {
+           // const singleImage = imageList[i];
+            //const imageItem = singleImage[imageIndex]
+            //console.log(singleImage)
         
-        //create li list for answer choices
-        cardAnswerList.forEach(choice => {
+        
+        //create li list for each answer choices
+        let i
+        for (i = 0; i < answerList.length; i++) {
+            const choice = answerList[i];
+
+            console.log("he")
             let answerItem = document.createElement("li")
-            let image = document.createElement('img')
             let answerDescription = card.rightAnswerInfo
-            answerItem.className = "answers"
+            let image = document.createElement('img')
+            image.src
             image.className = "sizing"
+            answerItem.className = "answers"
             showScore();
+            
+            //console.log(imageList)
 
-            console.log(imageChoice)
-            console.log(answerItem)
+            //console.log(imageList)
+            //console.log(answerItem)
 
-                //event click listener for each li answer choice
-                answerItem.addEventListener("click", evt => {
-                    const userAnswer = evt.target.innerText;
-                    //if user answer = right answer then
-                    if (card.isAnswerCorrect(userAnswer) === true) {
-                        answerItem.classList.add("answer-correct")
-                        answerText.classList.add("green")
-                        answerText.classList.remove("red")
-                        answerItem.classList.remove("answers")
-                        answerText.innerText = "Correct! Your Right"
-                        showDescription.classList.add("answer-container")
-                        showDescription.innerText = answerDescription
-                        score++;
-                    } else if (card.isAnswerCorrect(userAnswer) == false) {
-                        answerItem.classList.add("answer-wrong")
-                        answerItem.classList.remove("answers")
-                    } 
-                },{once: true})
+            //event click listener for each li answer choice
+            checkAnswer();
             
             answerItem.textContent = choice;
+            image.setAttribute('src', imageList[i])
+            answerItem.appendChild(image);
             answerList.append(answerItem);
-            image.setAttribute('src', "/images/Denali.png")
-            answerItem.appendChild(image)
-    })       
-}
+        }
+    }     
+//}
 displayQuestionCard();
 
 function showScore() {
@@ -174,3 +177,24 @@ backButton.addEventListener("click", evt => {
     displayQuestionCard();
 })
 
+function checkAnswer() {
+    answerItem.addEventListener("click", evt => {
+        const userAnswer = evt.target.innerText;
+        //const li = document.getElementsByName("li")
+        //console.log(li)
+        //if user answer = right answer then
+        if (card.isAnswerCorrect(userAnswer) === true) {
+            answerItem.classList.add("answer-correct")
+            answerText.classList.add("green")
+            answerText.classList.remove("red")
+            answerItem.classList.remove("answers")
+            answerText.innerText = "Correct! Your Right"
+            showDescription.classList.add("answer-container")
+            showDescription.innerText = answerDescription
+            score++;
+        } else if (card.isAnswerCorrect(userAnswer) == false) {
+            answerItem.classList.add("answer-wrong")
+            answerItem.classList.remove("answers")
+        } 
+    },{once: true})
+};
